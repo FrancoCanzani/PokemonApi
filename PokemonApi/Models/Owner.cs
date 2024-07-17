43f0a264785c6a4cc8118dfc37e29b7a4e80b1f0
Franco Canzani <francocanzani@gmail.com>
2024-07-17 23:47:24 +0200

    Adds models

---
+++ PokemonApi/Models/Owner.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonApi.Models
+{
+    public class Owner
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gym { get; set; }
+    }
+}
